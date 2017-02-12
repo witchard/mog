36f49dd95e98d80e5dbec329a96fd87986a63f8a
witchard <witchard@hotmail.co.uk>
2017-02-12 23:43:55 +0000

    Version bump 0.3 because i messed up the PyPI release...

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 setup(
   name = 'mog',
   packages = ['mog'],
-  version = '0.2',
+  version = '0.3',
   description = 'A different take on the UNIX tool cat',
   install_requires = ['mdv', 'pygments'],
   author = 'witchard',
   author_email = 'witchard@hotmail.co.uk',
   url = 'https://github.com/witchard/mog',
-  download_url = 'https://github.com/witchard/mog/tarball/0.1',
+  download_url = 'https://github.com/witchard/mog/tarball/0.3',
   keywords = ['terminal', 'highlighting', 'cat'],
   classifiers = [],
   entry_points = {'console_scripts': ['mog = mog:main']}
 )
 
+# DONT FORGET TO CHANGE DOWNLOAD_URL WHEN DOING A RELEASE!
 # Thanks to this guide: http://peterdowns.com/posts/first-time-with-pypi.html
