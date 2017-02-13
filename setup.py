eeabbcf06895218f5dd20986e9e42a934756cbdd
witchard <witchard@hotmail.co.uk>
2017-02-13 22:25:10 +0000

    Release 0.4

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'mog',
   packages = ['mog'],
-  version = '0.3',
+  version = '0.4',
   description = 'A different take on the UNIX tool cat',
   install_requires = ['mdv', 'pygments'],
   author = 'witchard',
   author_email = 'witchard@hotmail.co.uk',
   url = 'https://github.com/witchard/mog',
-  download_url = 'https://github.com/witchard/mog/tarball/0.3',
+  download_url = 'https://github.com/witchard/mog/tarball/0.4',
   keywords = ['terminal', 'highlighting', 'cat'],
   classifiers = [],
   entry_points = {'console_scripts': ['mog = mog:main']}
