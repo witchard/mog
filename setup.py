b6769d9b10cc19ba59510cef598a4083f84b301a
witchard <witchard@hotmail.co.uk>
2017-03-01 21:24:47 +0000

    Prep for release 0.5.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'mog',
   packages = ['mog'],
-  version = '0.4',
+  version = '0.5.0',
   description = 'A different take on the UNIX tool cat',
   install_requires = ['mdv', 'pygments'],
   author = 'witchard',
   author_email = 'witchard@hotmail.co.uk',
   url = 'https://github.com/witchard/mog',
-  download_url = 'https://github.com/witchard/mog/tarball/0.4',
+  download_url = 'https://github.com/witchard/mog/tarball/0.5.0',
   keywords = ['terminal', 'highlighting', 'cat'],
   classifiers = [],
   entry_points = {'console_scripts': ['mog = mog:main']}
@@ -16,3 +16,8 @@ setup(
 
 # DONT FORGET TO CHANGE DOWNLOAD_URL WHEN DOING A RELEASE!
 # Thanks to this guide: http://peterdowns.com/posts/first-time-with-pypi.html
+# Release with:
+#    git tag <version>
+#    git push --tags
+#    python setup.py sdist upload -r pypi
+
